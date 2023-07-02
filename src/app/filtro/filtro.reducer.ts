ff6764c2cc314369aad25a7d1d5e0caee591a074
Uriel Dávila <uri.davi.14@gmail.com>
2023-07-01 22:17:21 -0600

    Docs creado

--- src/app/filtro/filtro.reducer.ts
+++ src/app/filtro/filtro.reducer.ts
@@ -1,7 +1,7 @@
 import { createReducer, on } from '@ngrx/store';
 import { setFiltro, filtrosValidos } from './filtro.actions';
 
-export const initialState:filtrosValidos= 'todos';
+export const initialState:filtrosValidos= 'todos' as filtrosValidos;
 
 export const filtroReducer = createReducer<filtrosValidos>(
   initialState,
